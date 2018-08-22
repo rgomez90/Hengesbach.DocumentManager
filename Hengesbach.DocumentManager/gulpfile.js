61e8cbeb459f43b60244c56316a61afcc7cbe383
Rafael Gómez <rafitagd90@gmail.com>
2018-08-22 12:39:39 +0200

    Add project files.

---
+++ Hengesbach.DocumentManager/gulpfile.js
@@ -0,0 +1,19 @@
+/// <binding Clean='clean' />
+"use strict";
+
+var gulp = require("gulp"),
+  rimraf = require("rimraf"),
+  concat = require("gulp-concat"),
+  cssmin = require("gulp-cssmin"),
+  uglify = require("gulp-uglify");
+
+var paths = {
+  webroot: "./wwwroot/"
+};
+
+paths.js = paths.webroot + "js/**/*.js";
+paths.minJs = paths.webroot + "js/**/*.min.js";
+paths.css = paths.webroot + "css/**/*.css";
+paths.minCss = paths.webroot + "css/**/*.min.css";
+paths.concatJsDest = paths.webroot + "js/site.min.js";
+paths.concatCssDest = paths.webroot + "css/site.min.css";
---
+++ Hengesbach.DocumentManager/wwwroot/js/site.js
@@ -0,0 +1,57 @@
+﻿var files=[];
+
+var input=document.getElementById('file');
+input.style.opacity = 0;
+input.onchange= function (event) {
+    for (let i = 0; i < input.files.length; i++) {
+        const element = input.files[i];
+        files.push(element);       
+    }
+}
+
+function dropHandler(ev) {
+    console.log('File(s) dropped');
+  
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+  
+    if (ev.dataTransfer.items) {
+      // Use DataTransferItemList interface to access the file(s)
+      for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+        // If dropped items aren't files, reject them
+        if (ev.dataTransfer.items[i].kind === 'file') {
+          var file = ev.dataTransfer.items[i].getAsFile();
+          files.push(file);
+          alert('... file[' + i + '].name = ' + file.name);
+        }
+      }
+    } else {
+      // Use DataTransfer interface to access the file(s)
+      for (var i = 0; i < ev.dataTransfer.files.length; i++) {
+        alert('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
+      }
+    } 
+    
+    // Pass event to removeDragData for cleanup
+    removeDragData(ev)
+  }
+
+  function dragOverHandler(ev){
+      ev.preventDefault();
+  }
+
+  function selectData() {
+      alert("Select Data!!");
+    }
+
+    function removeDragData(ev) {
+        console.log('Removing drag data')
+      
+        if (ev.dataTransfer.items) {
+          // Use DataTransferItemList interface to remove the drag data
+          ev.dataTransfer.items.clear();
+        } else {
+          // Use DataTransfer interface to remove the drag data
+          ev.dataTransfer.clearData();
+        }
+      }
